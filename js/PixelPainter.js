83c446609018babdbcd681cfcd7f54723c8057c1
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-01-07 01:43:14 -1000

    added array of colors

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -39,7 +39,7 @@ let back = document.createElement('button');
 back.id='back';
 back.className='topButtons';
 back.innerHTML='Rewind';
-buttonRow.appendChild(back)
+buttonRow.appendChild(back);
 
 
 
@@ -69,7 +69,7 @@ let gridRow = document.createElement('div');
 gridRow.id='gridRow';
 pixelPainter.appendChild(gridRow);
 //palette stuff
-let palette = gridMe(3,7);
+let palette = gridMe(3,5);
 palette.id='palette';
 let paletteFlex=document.createElement('div');
 paletteFlex.id='paletteFlex';
@@ -94,11 +94,14 @@ selectFlex.appendChild(select)
 let pix = document.getElementsByClassName('pix');
 
 for(var i=0;i<pix.length;i++){
-    if(i<=20){
+    if(i<=14){
         pix[i].style.height = '30px';
         pix[i].style.width='30px';
     }
 }
+//palette colors
+let colors = ['red','rgb(255, 67, 0)','rgb(255, 174, 0)','rgb(255, 255, 0)','rgb(174, 255, 0)','rgb(63, 255, 0)','rgb(0, 255, 21)','rgb(0, 255, 242)','rgb(0, 144, 255)','rgb(0, 144, 255)','rgb(93, 0, 255)','rgb(255, 0, 216)','brown','black','white'];
+
 
 
 
